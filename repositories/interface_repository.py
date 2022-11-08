8fb6e2494530b056fb18a1370e549895afc9d6f4
David Franco <franco.misionticunal@gmail.com>
2022-11-08 00:13:36 -0500

    Transform objects ids finished

--- repositories/interface_repository.py
+++ repositories/interface_repository.py
@@ -1,7 +1,7 @@
 import json
 import pymongo
 import certifi
-from bson import ObjectId
+from bson import ObjectId, DBRef
 from typing import Generic, TypeVar, get_args
 
 T = TypeVar('T')
@@ -66,30 +66,82 @@ class InterfaceRepository(Generic[T]):
         return self.find_by_id(id_)
 
     def update(self, id_: str, item: T) -> dict:
-        pass
+        current_collection = self.data_base[self.collection]
+        _id = ObjectId(id_)
+        item = item.__dict__
+        update_item = {"$set": item}
+        document = current_collection.update_one({"_id": _id}, update_item)
+        return {"update-count": document.matched_count}
 
     def delete(self, id_: str) -> dict:
-        pass
+        current_collection = self.data_base[self.collection]
+        _id = ObjectId(id_)
+        result = current_collection.delete_one({"_id": _id})
+        return {"delete-count": result.deleted_count}
 
     def query(self, query: dict) -> list:
-        pass
+        current_collection = self.data_base[self.collection]
+        dataset = []
+        for document in current_collection.find(query):
+            document['_id'] = document['_id'].__str__()
+            document = self.transform_object_ids(document)
+            document = self.get_values_db_ref(document)
+            dataset.append(document)
+        return dataset
 
-    def query_aggregation(self, query:dict) -> list:
-        pass
+    def query_aggregation(self, query: dict) -> list:
+        current_collection = self.data_base[self.collection]
+        dataset = []
+        for document in current_collection.aggregate(query):
+            document['_id'] = document['_id'].__str__()
+            document = self.transform_object_ids(document)
+            document = self.get_values_db_ref(document)
+            dataset.append(document)
+        return dataset
 
-    def get_values_db_ref(self):
-        pass
+    # Mongo to Python
+    def get_values_db_ref(self, document: dict) -> dict:
+        for key in document.keys():
+            value = document.get(key)
+            if isinstance(value, DBRef):
+                collection_ref = self.data_base[value.collection]
+                _id = ObjectId(value.id)
+                document_ref = collection_ref.find({"_id": _id})
+                document_ref["_id"] = document_ref["_id"].__str__()
+                document[key] = document_ref
+                document[key] = self.get_values_db_ref(document[key])
+            elif isinstance(value, list) and len(value) > 0:
+                document[key] = self.get_values_db_ref_from_list(value)
+            elif isinstance(value, dict):
+                document[key] = self.get_values_db_ref(value)
+        return document
 
-    def get_values_db_ref_from_list(self):
-        pass
+    def get_values_db_ref_from_list(self, list_: list) -> list:
+        processed_list = []
+        collection_ref = self.data_base[list_[0]._id.collection]
+        for item in list_:
+            _id = ObjectId(item._id)
+            document_ref = collection_ref.find_one({"_id": _id})
+            document_ref["_id"] = document_ref["_id"].__str__()
+            processed_list.append(document_ref)
+        return processed_list
 
-    def transform_object_ids(self):
-        pass
-
-    def transform_refs(self):
-        pass
+    def transform_object_ids(self, document: dict) -> dict:
+        for key in document.keys():
+            value = document.get(key)
+            if isinstance(value, ObjectId):
+                document[key] = document[key].__str__()
+            elif isinstance(value, list) and len(value) > 0:
+                document[key] = self.format_list(value)
+            elif isinstance(value, dict):
+                document[key] = self.transform_object_ids(value)
+        return document
 
     def format_list(self):
+        pass
+
+    # Python to Mongo
+    def transform_refs(self):
         pass
 
     def object_to_db_ref(self):
